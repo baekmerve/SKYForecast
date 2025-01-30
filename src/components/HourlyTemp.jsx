cb3b9f87229ab8fb060b3e534dedc6c025345558
baekmerve <baekmerve@gmail.com>
2025-01-31 01:50:20 +0900

    char ui edit

--- src/components/HourlyTemp.jsx
+++ src/components/HourlyTemp.jsx
@@ -3,12 +3,20 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "./ui/card";
-import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 import { format } from "date-fns";
 
 const HourlyTemp = ({ data }) => {
   const chartData = data.list.slice(0, 8).map((item) => ({
+    //every 3 hours
     time: format(new Date(item.dt * 1000), "ha"), // ha: hour + am/pm
     temp: Math.round(item.main.temp),
     feels_like: Math.round(item.main.feels_like),
@@ -21,66 +29,69 @@ const HourlyTemp = ({ data }) => {
       </CardHeader>
       <CardContent>
         <div className="h-[200px] w-full">
-          {/* //note: make graph responsive */}
+          {/*  make graph responsive */}
           <ResponsiveContainer width={"100%"} height={"100%"}>
             <LineChart data={chartData}>
               <XAxis
                 dataKey="time"
                 stroke="#888888"
                 fontSize={12}
-                tickLine={false}
-                axisLine={false}
+                tickLine={true}
+                axisLine={true}
               />
               <YAxis
                 stroke="#888888"
                 fontSize={12}
-                tickLine={false}
-                axisLine={false}
+                tickLine={true}
+                axisLine={true}
                 tickFormatter={(value) => `${value}°`}
               />
               {/* tooltip */}
 
-              <Tooltip 
-              content={({active, payload})=>{
-                if (active && payload && payload.length){
-                  return (
-                    <div className="rounded-lg border bg-background p-2 shadow-sm">
-                      <div className="grid grid-cols-2 gap-2">
-                        <div className="flex flex-col">
-                          <span className="text-[0.70rem] uppercase text-muted-foreground">
-                            Temperature
-                          </span>
-                          <span className="font-bold">{payload[0].value}°</span>
-                        </div>
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload && payload.length) {
+                    return (
+                      <div className="rounded-lg border bg-background p-2 shadow-sm">
+                        <div className="grid grid-cols-2 gap-2">
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              Temperature
+                            </span>
+                            <span className="font-bold">
+                              {payload[0].value}°
+                            </span>
+                          </div>
 
-                        <div className="flex flex-col">
-                          <span className="text-[0.70rem] uppercase text-muted-foreground">
-                            Feels Like
-                          </span>
-                          <span className="font-bold">{payload[1].value}°</span>
+                          <div className="flex flex-col">
+                            <span className="text-[0.70rem] uppercase text-muted-foreground">
+                              Feels Like
+                            </span>
+                            <span className="font-bold">
+                              {payload[1].value}°
+                            </span>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                }
-                return null;
-              }}
-              
+                    );
+                  }
+                  return null;
+                }}
               />
               <Line
                 type="monotone"
                 dataKey="temp"
-                stroke="#2563eb"
+                stroke="#8884d8"
                 strokeWidth={2}
-                dot={false}
+                activeDot={{ r: 5 }}
               />
               <Line
                 type="monotone"
                 dataKey="feels_like"
-                stroke="#64748b"
+                stroke="#82ca9d"
                 strokeWidth={2}
-                dot={false}
-                strokeDasharray="5 5"
+                strokeDasharray="4 4"
+                activeDot={{ r: 5 }}
               />
             </LineChart>
           </ResponsiveContainer>
